9730d9c8749080fde3da15cbf5a9d90062f0251e
Jody Klymak <jklymak@gmail.com>
2024-01-21 15:37:48 -0800

    gendata for Bute3d94Stop5.0d:     Pickup from Bute3d94, stopped after 5.0 days.

--- input/runAll.py
+++ input/runAll.py
@@ -6,7 +6,7 @@ timest='19:20:00'
 
 runModelName = 'runModelCedar.sh'
 
-for todo in [f'Bute3d{runno}' for runno in range(93, 94)]:
+for todo in [f'Bute3d{runno}' for runno in range(94, 98)]:
     outstr = f"{todo} queued "
     res = subprocess.check_output(["sbatch", f"--job-name={todo}",
                                    f"--time={timest}",
